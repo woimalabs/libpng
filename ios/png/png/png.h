315321bb8b1160f766dd165fca15807f6049aca0
Antti Peuhkurinen <antti.peuhkurinen@woimasolutions.com>
2013-11-03 14:01:37 +0200

    ios project files

---
+++ ios/png/png/png.h
@@ -0,0 +1,13 @@
+//
+//  png.h
+//  png
+//
+//  Created by Antti Peuhkurinen on 3.11.2013.
+//  Copyright (c) 2013 Woima Solutions Oy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface png : NSObject
+
+@end
---
+++ ios/png/png/png.m
@@ -0,0 +1,13 @@
+//
+//  png.m
+//  png
+//
+//  Created by Antti Peuhkurinen on 3.11.2013.
+//  Copyright (c) 2013 Woima Solutions Oy. All rights reserved.
+//
+
+#import "png.h"
+
+@implementation png
+
+@end
